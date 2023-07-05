67dc42d7a04ac57a4b5163c59cc353e9ebfc16e4
j s <grnwtmry@gmail.com>
2023-07-06 02:58:39 +0300

    login validation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,24 +6,45 @@ import { Link } from "react-router-dom";
 
 function Login() {
   const [data, setData] = useState({ userName: "", password: "" });
+  const [passwordErrors, setPasswordErrors] = useState([]);
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+  const validatePassword = (password) => {
+    const errors = [];
+    if (!passwordRegex.test(password)) {
+      if (password.length < 6) {
+        errors.push("Password should be at least 6 characters long");
+      }
+      if (!/(?=.*[A-Z])/.test(password)) {
+        errors.push("Password should contain at least one uppercase letter");
+      }
+      if (!/(?=.*\d)/.test(password)) {
+        errors.push("Password should contain at least one number");
+      }
+      if (!/(?=.*[@$!%*?&])/.test(password)) {
+        errors.push("Password should contain at least one special character");
+      }
+    }
+    setPasswordErrors(errors);
+    return errors.length === 0;
+  };
   const tempUserData = {
     userName: "emma_smith",
     password: "password123",
   };
   const handleSubmit = (e) => {
-    console.log("245245");
     e.preventDefault();
+    if (validatePassword(data.password) && data.password.length >= 3) {
     axios
-      .post("http://localhost:1000/users/login", data)
+      .post("http://localhost:1000/users/login", tempUserData)
       .then((res) => localStorage.setItem("token", res.data))
       .catch((err) => console.log(err));
-  };
+  }};
   return (
     <>
       <div className={styles.logoBackground}>
         <div className={styles.logoContainer}>
           <BsMusicNote
-            style={{ marginBottom: "8px" /*marginBottom:"1.5vh"*/ }}
+            style={{ marginBottom: "8px"}}
             size={103}
           />
           <span className={styles.logoText}> Chexi</span>
@@ -31,9 +52,10 @@ function Login() {
       </div>
 
       <div className={styles.loginContainer}>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form}  onSubmit={handleSubmit} noValidate>
           <span>Login</span>
-          <div>
+          <div className={styles.inputContainer}>
+            <div>
           <label htmlFor="username">User name</label>
           <input
             type="text"
@@ -43,22 +65,33 @@ function Login() {
             onChange={(e) =>
               setData((prev) => ({ ...prev, userName: e.target.value }))
             }
+            className={data.userName.trim().length >= 3? styles.greenBorder : data.userName.trim().length >= 1 && styles.redBorder}
           />
+          {(data.userName.trim().length < 3 && data.userName.trim().length >= 1) && <span className={styles.error}>User name should contain at least 3 chracters</span>}
           </div>
-          <div>
+          <div >
             <label htmlFor="password">Password</label>
           <input
+          className={`${passwordErrors.length > 0 ? styles.redBorder : data.password.trim().length > 6 && styles.greenBorder}`}
           id="password"
             type="password"
             placeholder="password"
+            pattern={passwordRegex.toString()}
             value={data.password}
-            onChange={(e) =>
-              setData((prev) => ({ ...prev, password: e.target.value }))
+            onChange={(e) =>  {
+              setData((prev) =>  ({ ...prev, password: e.target.value }));
+              validatePassword(e.target.value)
+            }
             }
           />
+          {passwordErrors.length > 0 && (
+                <span className={styles.error}>
+                  {passwordErrors[passwordErrors.length - 1]}
+                </span>)}
           </div>
-          <button type="submit">Login</button>
-          <Link to={"/home"}>enter as a guest</Link>
+          </div>
+          <button type="submit" >Login</button>
+          <Link  to={"/home"}>Continue as a guest</Link>
         </form>
       </div>
     </>
