c1a6ca4f0be79984902ae49db6175b0a27abfd53
j s <grnwtmry@gmail.com>
2023-08-06 01:43:55 +0300

    ....

--- src/pages/FavoriteArtists/index.jsx
+++ src/pages/FavoriteArtists/index.jsx
@@ -2,7 +2,7 @@ import styles from "./style.module.css";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { Suspense, useContext, useEffect, useState } from "react";
 import Playlist from "../../components/playlist";
-import NewPlaylistOrArtist from "../../components/popUps/newPlaylistOrArtist";
+import NewPlaylistOrArtist from "../../components/popUps/NewPlaylistOrArtist";
 import HandlePlayingSongContext from "../../contexts/HandlePlayingSong";
 import axios from "axios";
 import api from "../../apiCalls/apiCalls"
